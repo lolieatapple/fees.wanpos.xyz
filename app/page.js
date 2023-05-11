aacb50999aa03730fd12224fd9d2b3cef01b09f7
lolieatapple <lolieatapple@gmail.com>
2023-05-11 17:13:59 +0800

    update for static

--- app/page.js
+++ app/page.js
@@ -372,14 +372,12 @@ export default function Home() {
         downloadCSV(csv, 'cross-chain-fees_'+ targetFee.toString()+'_'+ (new Date().toISOString().replace(':', '_')) +'.csv');
       }}>Generate CSV</button>
       {/* <button>Generate JSON</button> */}
-      {
-        coinPrices.length > 0 && <button onClick={async () => {
-          let fees = await fetch("/api/currentFee");
-          fees = await fees.json();
-          const csv = convertArrayOfObjectsToCSV('TokenPairId,From,To,Symbol,Decimals,NetworkFee,IsPercent,OperationFee,IsPercent', fees);
-          downloadCSV(csv, 'cross-chain-fees_'+ targetFee.toString()+'_'+ (new Date().toISOString().replace(':', '_')) +'.csv');
-        }}>Download Current Fees CSV</button>
-      }
+      <button onClick={async () => {
+        let fees = await fetch("/api/currentFee");
+        fees = await fees.json();
+        const csv = convertArrayOfObjectsToCSV('TokenPairId,From,To,Symbol,Decimals,NetworkFee,IsPercent,OperationFee,IsPercent', fees);
+        downloadCSV(csv, 'cross-chain-fees_'+ targetFee.toString()+'_'+ (new Date().toISOString().replace(':', '_')) +'.csv');
+      }}>Download Current Fees CSV</button>
     </div>
   );
 }
