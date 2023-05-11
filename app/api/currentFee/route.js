4d0774d096c7a42d135c08eacb8627ec6177280e
lolieatapple <lolieatapple@gmail.com>
2023-05-11 17:30:58 +0800

    limit max concurrent

--- app/api/currentFee/route.js
+++ app/api/currentFee/route.js
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 import iWanClient from "@wandevs/iwan-sdk";
-import { tokenPair } from '@/constants/tokenPair';
-import { chainIds } from '@/constants/chainConstant';
+import { tokenPair } from "@/constants/tokenPair";
+import { chainIds } from "@/constants/chainConstant";
 
 let option = {
   url: "api.wanchain.org",
@@ -20,50 +20,76 @@ export async function GET(req, res) {
       option
     );
 
-    let promises = tokenPair.map(async (pair) => {
-      let from;
-      let to;
-      let symbol;
-      try {
-        from = chainIds.find(v => Number(v[0]) === Number(pair.fromChainID))[1];
-        to = chainIds.find(v => Number(v[0]) === Number(pair.toChainID))[1];
-        symbol = pair.ancestorSymbol;
-    
-        const [ret, ret2] = await Promise.all([
-          iWan.estimateCrossChainNetworkFee(from, to, { tokenPairID: pair.id }),
-          iWan.estimateCrossChainOperationFee(from, to, { tokenPairID: pair.id })
-        ]);
-    
-        console.log(from, to, ret, ret2);
-    
-        return {
-          tokenPairId: pair.id,
-          from,
-          to,
-          symbol: pair.ancestorSymbol,
-          decimals: pair.decimals,
-          networkFee: ret.value,
-          networkFeeIsPercent: ret.isPercent,
-          operationFee: ret2.value,
-          operationFeeIsPercent: ret2.isPercent,
-        };
-      } catch (error) {
-        console.log(from, to, symbol, error);
-        return {
-          tokenPairId: pair.id,
-          from,
-          to,
-          symbol: pair.ancestorSymbol,
-          decimals: pair.decimals,
-          networkFee: 'failed',
-          networkFeeIsPercent: 'failed',
-          operationFee: 'failed',
-          operationFeeIsPercent: 'failed',
-        };
-      }
+    // 定义最大并发数量
+    const MAX_CONCURRENT = 10;
+
+    // 定义一个队列来存放待处理的任务
+    let taskQueue = [];
+
+    // 遍历 tokenPair，为每个 pair 创建一个任务，然后将任务添加到队列中
+    tokenPair.forEach((pair) => {
+      taskQueue.push(async () => {
+        let from, to, symbol;
+        try {
+          from = chainIds.find(
+            (v) => Number(v[0]) === Number(pair.fromChainID)
+          )[1];
+          to = chainIds.find((v) => Number(v[0]) === Number(pair.toChainID))[1];
+          symbol = pair.ancestorSymbol;
+
+          const [ret, ret2] = await Promise.all([
+            iWan.estimateCrossChainNetworkFee(from, to, {
+              tokenPairID: pair.id,
+            }),
+            iWan.estimateCrossChainOperationFee(from, to, {
+              tokenPairID: pair.id,
+            }),
+          ]);
+
+          console.log(from, to, ret, ret2);
+
+          return {
+            tokenPairId: pair.id,
+            from,
+            to,
+            symbol: pair.ancestorSymbol,
+            decimals: pair.decimals,
+            networkFee: ret.value,
+            networkFeeIsPercent: ret.isPercent,
+            operationFee: ret2.value,
+            operationFeeIsPercent: ret2.isPercent,
+          };
+        } catch (error) {
+          console.log(from, to, symbol, error);
+          return {
+            tokenPairId: pair.id,
+            from,
+            to,
+            symbol: pair.ancestorSymbol,
+            decimals: pair.decimals,
+            networkFee: "failed",
+            networkFeeIsPercent: "failed",
+            operationFee: "failed",
+            operationFeeIsPercent: "failed",
+          };
+        }
+      });
     });
-    
-    allFees = await Promise.all(promises);    
+
+    // 定义一个函数，该函数将处理队列中的任务，直到队列为空
+    const processQueue = async () => {
+      let results = [];
+      while (taskQueue.length > 0) {
+        // 获取下一组待处理的任务
+        let tasks = taskQueue.splice(0, MAX_CONCURRENT);
+        // 并行处理这些任务，并将结果添加到结果数组中
+        results.push(...(await Promise.all(tasks.map((task) => task()))));
+      }
+      return results;
+    };
+
+    // 处理队列中的任务
+    allFees = await processQueue();
 
     return NextResponse.json(allFees);
   } catch (error) {
