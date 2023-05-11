c16532967f2e8606ae2f3c8a46de49cf3dd85f40
lolieatapple <lolieatapple@gmail.com>
2023-05-11 11:41:39 +0800

    modify to multi 1.8

--- app/api/recordFeeRate/route.js
+++ app/api/recordFeeRate/route.js
@@ -10,15 +10,15 @@ export async function GET(req, res) {
   try {
     console.log('fetching doge fee rate from blockcypher api...');
     let ret = await axios.get(dogeUrl, {timeout: 5000});
-    let dogeFeeRate = ((ret.data.high_fee_per_kb + ret.data.medium_fee_per_kb) / 2 * 1.1).toFixed(0);
+    let dogeFeeRate = ((ret.data.high_fee_per_kb + ret.data.medium_fee_per_kb) / 2 * 1.8).toFixed(0);
 
     console.log('fetching btc fee rate from blockcypher api...');
     ret = await axios.get(btcUrl, {timeout: 5000});
-    let btcFeeRate = ((ret.data.high_fee_per_kb + ret.data.medium_fee_per_kb) / 2 * 1.1).toFixed(0);
+    let btcFeeRate = ((ret.data.high_fee_per_kb + ret.data.medium_fee_per_kb) / 2 * 1.8).toFixed(0);
 
     console.log('fetching ltc fee rate from blockcypher api...');
     ret = await axios.get(ltcUrl, {timeout: 5000});
-    let ltcFeeRate = ((ret.data.high_fee_per_kb + ret.data.medium_fee_per_kb) / 2 * 1.1).toFixed(0);
+    let ltcFeeRate = ((ret.data.high_fee_per_kb + ret.data.medium_fee_per_kb) / 2 * 1.8).toFixed(0);
 
     console.log('doge fee rate', dogeFeeRate, 'btc fee rate', btcFeeRate, 'ltc fee rate', ltcFeeRate);
 
