158295a4c6a92dc5b0fb24d462b5f9bf1feacdff
lolieatapple <lolieatapple@gmail.com>
2023-05-12 11:19:20 +0800

    update

--- app/api/test/route.js
+++ app/api/test/route.js
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
 
-// export async function GET(req, res) {
-//   console.log('GET John Doe');
-//   return NextResponse.json({ name: 'GET John Doe' })
-// }
+export async function GET(req, res) {
+  console.log('GET John Doe');
+  return NextResponse.json({ name: 'GET John Doe' })
+}
 
 export async function POST(req, res) {
   console.log('POST John Doe');
